f89c570bdd4f4d519c5b1037580c2ea386b92c36
BRUNO <BRUNO@BRUNO-PC>
2015-05-25 20:58:34 -0300

    teste 3 jogo da velha

---
+++ digitacacao/src/aula/Asterisco.java
@@ -0,0 +1,17 @@
+package aula;
+
+public class Asterisco {
+
+	public static void main(String[] args) {
+		int tamanho= 10;
+		for(int x=0;x<tamanho; x++){
+			for(int i=0; i<tamanho;i++){
+				System.out.print("xy");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
